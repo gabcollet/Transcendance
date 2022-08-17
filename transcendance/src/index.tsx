57aae1b8855de3fdf2149e4b5fc37cb17fd3865b
mafortin <mafortin@student.42quebec.com>
2022-08-17 16:13:09 -0400

    added nav bar + user icon

--- transcendance/src/index.tsx
+++ transcendance/src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./NavBar.css";
 import App from "./App";
+import TopBanner from "./NavBar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div className="header"></div>
+    <div className="top-banner">
+      <TopBanner></TopBanner>
+    </div>
     <div className="middle">
       <div className="content">
         <App></App>
