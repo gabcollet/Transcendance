f0d40a299c8cef8e55f3fbdfe21027412d5642ad
mafortin <hpst@protonmail.com>
2022-08-23 19:13:54 -0400

    removed old components + added file structure

--- transcendance/src/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const Footer = () => {
-  return <div className="footer-bar"></div>;
-};
-
-export default Footer;
--- transcendance/src/index.tsx
+++ transcendance/src/index.tsx
@@ -1,25 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./NavBar.css";
-import "./Footer.css";
 import App from "./App";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <div className="top-banner">
-      <NavBar></NavBar>
-    </div>
-    <div className="content">
-      <App></App>
-    </div>
-    <div className="footer">
-      <Footer></Footer>
-    </div>
-  </React.StrictMode>
-);
+root.render(<div>Hello World</div>);
