cc146a790cae7561b63f6840ab705fc7d9a33f04
Maxime Mondello <maxime@quebecnumerique.com>
2022-08-29 15:24:48 -0400

    Login

--- transcendance/src/components/Menu/MenuButton.tsx
+++ transcendance/src/components/Menu/MenuButton.tsx
@@ -1,12 +1,11 @@
 import "./MenuButton.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { MouseEventHandler, useState } from "react";
+import { useState } from "react";
 
 interface _Button {
   icon: any;
   className: string;
   iconClassName: string;
-  //   i: number;
 }
 
 const MenuButton = (props: _Button) => {
