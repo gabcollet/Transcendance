a19edbab2a3b634a2f1ee86c48949ff0bec5c71b
Maxime Mondello <maxime@quebecnumerique.com>
2022-09-07 09:38:09 -0400

    Merged Max into Api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { PongController } from './pong/pong.controller';
+import { PongService } from './pong/pong.service';
+import { PongModule } from './pong/pong.module';
+import { ChatController } from './chat/chat.controller';
+import { ChatModule } from './chat/chat.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [AuthModule, PongModule, ChatModule, UsersModule],
+  controllers: [PongController, ChatController, UsersController],
+  providers: [PongService, UsersService],
 })
 export class AppModule {}
