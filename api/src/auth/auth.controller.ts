99d660e97e8265f30fba29999a8894b04688b8ee
armorine <maxime@quebecnumerique.com>
2022-09-08 12:59:17 -0400

    .

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -10,10 +10,6 @@ import {
 import Passport from 'passport-42';
 import { useParams } from 'react-router-dom';
 
-const CLIENT_ID =
-  '3187aa6aec15b19df251d4424b7bc8f7fc761603b4bae21e049ce800e217a812';
-const SECRET =
-  '94ecaff84914481f0bbd67e14b57bcbd65d8d8ea7f1f5918870e02c9ae4e04dc';
 
 @Controller('auth')
 export class AuthController {
