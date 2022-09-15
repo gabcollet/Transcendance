f6a43d3d0db7ae48ae0a8e46c5519e608142cd38
gcollet <gab_collet@hotmail.com>
2022-09-15 16:01:44 -0400

    pull main in pong-back

--- app/src/Pages/PongRoom.tsx
+++ app/src/Pages/PongRoom.tsx
@@ -6,7 +6,7 @@ import io from 'socket.io-client'
 
 export let roomID: string;
 export let pID: number;
-export const socket = io("10.13.11.2:9006");
+export const socket = io("10.13.11.2:6006");
 
 const PongRoom = () => {
 
