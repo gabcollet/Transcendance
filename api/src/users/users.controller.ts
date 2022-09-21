4fde52bc3593c55eb0c683e200defe38e12dcc36
LPAube <lpaube@hotmail.com>
2022-09-21 10:07:12 -0400

    Prisma migration done; ready to merge

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -26,73 +26,37 @@ export class UsersController {
     return this.usersService.getAllUsers();
   }
 
-  @Post('createuser')
-  async createUser(@Body() body: UserDto) {
-    return await this.usersService.create(
-      456,
-      "LP",
-      "lauboiii"
-    );
+  // Get path of user's image from DB
+  @UseGuards(JwtAuthGuard)
+  @Get(':name/img')
+  getUserImg(@Req() req: Request, @Param() params) {
+    return this.usersService.getUserImage(params.name);
   }
-}
-
-//   @Post()
-//   async createUser(@Body() body) {
-//     console.log(body);
-//     return await this.usersService.create(
-//       body.intraId,
-//       body.displayname,
-//       body.username,
-//       body.picture,
-//       body.wins,
-//       body.losses,
-//       );
-//     }
-
-//   // Get path of user's image from DB
-//   @UseGuards(JwtAuthGuard)
-//   @Get(':name/img')
-//   getUserImg(@Req() req: Request, @Param() params) {
-//     return this.usersService.getUserImage(params.name);
-//   }
 
 //   // Get displayname of user
-//   @UseGuards(JwtAuthGuard)
-//   @Get(':name/displayname')
-//   getDisplayName(@Req() req: Request, @Param() params) {
-//     return this.usersService.getDisplayName(params.name);
-//   }
+  @UseGuards(JwtAuthGuard)
+  @Get(':name/displayname')
+  getDisplayName(@Req() req: Request, @Param() params) {
+    return this.usersService.getDisplayName(params.name);
+  }
 
 //   // Get online status of user
-//   @UseGuards(JwtAuthGuard)
-//   @Get(':name/status')
-//   getStatus(@Req() req: Request, @Param() params) {
-//     return this.usersService.getStatus(params.name);
-//   }
+  @UseGuards(JwtAuthGuard)
+  @Get(':name/status')
+  getStatus(@Req() req: Request, @Param() params) {
+    return this.usersService.getStatus(params.name);
+  }
 
-//   // Get all time wins of user
-//   @UseGuards(JwtAuthGuard)
-//   @Get(':name/wins')
-//   getAllTimeWins(@Req() req: Request, @Param() params) {
-//     return this.usersService.getAllTimeWins(params.name);
-//   }
-//   // Get all time losses of user
-//   @UseGuards(JwtAuthGuard)
-//   @Get(':name/losses')
-//   getAllTimeLosses(@Req() req: Request, @Param() params) {
-//     return this.usersService.getAllTimeLosses(params.name);
-//   }
-
-//   // Fetch user image from its directory and return it as a file
-//   @Get('storedimg/:name')
-//   getFile(@Param() params, @Res() res: Response) {
-//     const file = createReadStream(join(process.cwd(), 'img', params.name));
-//     file.pipe(res);
-//   }
-//   // Update user for testing purposes
-//   @UseGuards(JwtAuthGuard)
-//   @Post('profile/update')
-//   updateImg(@Req() req: Request) {
-//     return this.usersService.updateImg(req.user);
-//   }
-// }
+  // Get all time wins of user
+  @UseGuards(JwtAuthGuard)
+  @Get(':name/wins')
+  getAllTimeWins(@Req() req: Request, @Param() params) {
+    return this.usersService.getAllTimeWins(params.name);
+  }
+  // Get all time losses of user
+  @UseGuards(JwtAuthGuard)
+  @Get(':name/losses')
+  getAllTimeLosses(@Req() req: Request, @Param() params) {
+    return this.usersService.getAllTimeLosses(params.name);
+  }
+}
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -44,9 +44,6 @@ export class UsersService {
   async findCreateUser(profile: Profile) {
     const { id, username, photos } = profile;
 
-    // const user = await this.prisma.user.findUnique({
-    //   where: { username: username }
-    // });
     const user = await this.findByUsername(username);
 
     if (!user) {
@@ -71,99 +68,31 @@ export class UsersService {
     return user;
   }
 
+// PROFILE REQUESTS
+
+  async getUserImage(username: string) {
+    const user = await this.findByUsername(username);
+    return user ? user.picture : null;
+  }
+
+  async getDisplayName(username: string) {
+    const user = await this.findByUsername(username);
+    return user ? user.displayname : null;
+  }
+
+  async getStatus(username: string) {
+    const user = await this.findByUsername(username);
+    return user ? user.status : null;
+  }
+
+  async getAllTimeWins(username: string) {
+    const user = await this.findByUsername(username);
+    return user ? user.wins : null;
+  }
+
+  async getAllTimeLosses(username: string) {
+    const user = await this.findByUsername(username);
+    return user ? user.losses : null;
+  }
+
 }
-
-//   findById(id: number) {
-//     return this.usersRepository.findOneBy({ id });
-//   }
-
-//   create(
-//     intraId: number,
-//     displayname: string,
-//     username: string,
-//     picture: string,
-//     wins?: number,
-//     losses?: number,
-//   ) {
-//     const user = this.usersRepository.create({
-//       intraId,
-//       displayname,
-//       username,
-//       picture,
-//       wins: 0,
-//       losses: 0,
-//     });
-
-//     return this.usersRepository.save(user);
-//   }
-
-//   async findCreateUser(profile: Profile) {
-//     const { id, username, photos } = profile;
-
-//     const user = await this.usersRepository.findOne({
-//       where: { username: username },
-//     });
-
-//     if (!user) {
-//       this.logger.log('*** User Not Found... Adding New User to Database***');
-//       return this.create(id, username, username, photos[0].value);
-//     }
-
-//     if (user.intraId == id) {
-//       this.logger.log('Found user: ' + username + ' with id: ' + user.id);
-//       return user;
-//     } else {
-//       throw new UnauthorizedException(
-//         'Resquesting Client Credentials Mismatch',
-//       );
-//     }
-//   }
-
-//   // Profile requests
-//   async getUserImage(username: string) {
-//     const user = await this.usersRepository.findOne({
-//       where: { username: username }
-//     });
-//     return user ? user.picture : null;
-//   }
-
-//   async getDisplayName(username: string) {
-//     const user = await this.usersRepository.findOne({
-//       where: { username: username }
-//     });
-//     return user ? user.displayname : null;
-//   }
-
-//   async getStatus(username: string) {
-//     const user = await this.usersRepository.findOne({
-//       where: { username: username }
-//     });
-//     return user ? user.status : null;
-//   }
-
-//   async getAllTimeWins(username: string) {
-//     const user = await this.usersRepository.findOne({
-//       where: {username: username }
-//     });
-//     return user ? user.wins : null;
-//   }
-
-//   async getAllTimeLosses(username: string) {
-//     const user = await this.usersRepository.findOne({
-//       where: {username: username }
-//     });
-//     return user ? user.losses : null;
-//   }
-
-//   async updateImg(username: any) {
-//     await this.usersRepository.update({
-//       username: username
-//     },
-//     {
-//       picture: "http://localhost:3030/users/storedimg/gcollet.jpg",
-//       wins: 3,
-//       losses: 2
-//     });
-//     return this.findAll();
-//   }
-// }
