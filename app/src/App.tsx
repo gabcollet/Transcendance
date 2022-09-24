7668210d8fbbc578b192c53e30eabc6c03d18a55
LPAube <lpaube@hotmail.com>
2022-09-24 08:22:14 -0400

    Added userContext

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,14 +7,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PongRoom from "./Pages/PongRoom";
 import Pong from "./components/Pong/Pong";
 import Cookies from "js-cookie";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import { fetchText } from "./components/Profile/FetchValue";
 import { SearchBar } from "./components/Profile/SearchBar";
 
+export const UserContext = React.createContext("");
+
 const App = () => {
   let [background, setBackground] = useState("root-default");
-  const [profileUsername, setProfileUsername] = useState("test");
+  const [profileUsername, setProfileUsername] = useState("USER NOT LOADED");
+  // const [profileUsername, setProfileUsername] = useState("test");
   // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
@@ -25,8 +28,6 @@ const App = () => {
     setBackground(newClassName);
   };
 
-  const [username, setUsername] = useState("USER NOT LOADED");
-
   useEffect(() => {
     async function getUsername() {
       await fetch("http://localhost:3030/profile/username", {
@@ -36,60 +37,64 @@ const App = () => {
         },
       })
         .then((res) => res.text())
-        .then((data) => setUsername(data));
+        .then((data) => setProfileUsername(data));
     }
     getUsername();
   }, []);
 
   return (
-    <div className={background}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login onChangeBg={changeBG}></Login>} />
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/Menu" element={<Menu />} />
-            <Route path="/PongRoom" element={<PongRoom />} />
-            <Route path="/Pong" element={<Pong />} />
-            <Route
-              path="/Spectate"
-              element={
-                <div style={{ color: "white", fontSize: "75px" }}>SPECTATE</div>
-              }
-            />
-            <Route
-              path="/Profile"
-              element={
-                <div style={{ color: "white", fontSize: "75px" }}>
-                  <Profile username={username} />
-                </div>
-              }
-            />
-            <Route
-              path="/Leaderboard"
-              element={
-                <div style={{ color: "white", fontSize: "75px" }}>
-                  LEADERBOARD
-                </div>
-              }
-            />
-            <Route
-              path="/Achievment"
-              element={
-                <div style={{ color: "white", fontSize: "75px" }}>
-                  ACHIEVMENT
-                </div>
-              }
-            />
-            <Route
-              path="/Chat"
-              element={
-                <div style={{ color: "white", fontSize: "75px" }}>CHAT</div>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
-    </div>
+    <UserContext.Provider value={profileUsername}>
+      <div className={background}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login onChangeBg={changeBG}></Login>} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/Menu" element={<Menu />} />
+              <Route path="/PongRoom" element={<PongRoom />} />
+              <Route path="/Pong" element={<Pong />} />
+              <Route
+                path="/Spectate"
+                element={
+                  <div style={{ color: "white", fontSize: "75px" }}>
+                    SPECTATE
+                  </div>
+                }
+              />
+              <Route
+                path="/Profile"
+                element={
+                  <div style={{ color: "white", fontSize: "75px" }}>
+                    <Profile username={profileUsername} />
+                  </div>
+                }
+              />
+              <Route
+                path="/Leaderboard"
+                element={
+                  <div style={{ color: "white", fontSize: "75px" }}>
+                    LEADERBOARD
+                  </div>
+                }
+              />
+              <Route
+                path="/Achievment"
+                element={
+                  <div style={{ color: "white", fontSize: "75px" }}>
+                    ACHIEVMENT
+                  </div>
+                }
+              />
+              <Route
+                path="/Chat"
+                element={
+                  <div style={{ color: "white", fontSize: "75px" }}>CHAT</div>
+                }
+              />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </UserContext.Provider>
   );
 };
 
