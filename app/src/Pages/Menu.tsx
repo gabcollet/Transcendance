e03d5461e90340ab1fbb5f1f3a3523a2475eea4b
gcollet <gab_collet@hotmail.com>
2022-09-29 16:38:07 -0400

    Status online/offline now work

--- app/src/Pages/Menu.tsx
+++ app/src/Pages/Menu.tsx
@@ -1,11 +1,9 @@
 import MenuWheel from "../components/Menu/MenuWheel";
 import Cookies from "js-cookie";
 import { Menu_ } from "../interfaces";
-import { socket } from "./PongRoom";
 
 const Menu = (props: Menu_): any => {
   const isLogged = Cookies.get("logged");
-  // socket.emit('online');
 
   return isLogged ? (
     <MenuWheel
