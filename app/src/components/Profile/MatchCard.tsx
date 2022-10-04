40d2a369b913b7f3d477ff925ff37356647383ab
LPAube <lpaube@hotmail.com>
2022-10-03 22:39:56 -0400

    Added proper css to match history of profile

--- app/src/components/Profile/MatchCard.tsx
+++ app/src/components/Profile/MatchCard.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect, useContext } from "react";
 import MatchCardStyle from "./MatchCard.module.css";
 import { fetchObject, fetchText } from "./FetchValue";
+import { ProfileContext } from "../../App";
 
 export const MatchCard = (props: any) => {
+  const profileName = useContext(ProfileContext);
+
   const [winnerUser, setWinnerUser] = useState(Object);
   const [loserUser, setLoserUser] = useState(Object);
 
@@ -11,20 +14,44 @@ export const MatchCard = (props: any) => {
     fetchObject("users/" + props.match.loser, setLoserUser);
   }, [props.match]);
 
+  let winOrLose = "";
+  if (profileName === props.match.winner) {
+    winOrLose = "W";
+  } else if (profileName === props.match.loser) {
+    winOrLose = "L";
+  }
+
   return (
-    <div className={MatchCardStyle["match-content-individual"]}>
-      <div className={MatchCardStyle["match-id"]}>
-        <div className={MatchCardStyle["me-id"]}>
-          <img src={winnerUser.picture} alt={winnerUser.username} />
-          <h4>{winnerUser.username}</h4>
-        </div>
-        <h3>
+    <div className={MatchCardStyle["match-container"]}>
+      <div className={MatchCardStyle["winner-container"]}>
+        <img
+          className={`${MatchCardStyle["winner-picture"]} ${MatchCardStyle["match-picture"]}`}
+          src={winnerUser.picture}
+          alt={winnerUser.username}
+        />
+        <h4
+          className={`${MatchCardStyle["winner-name"]} ${MatchCardStyle["match-name"]}`}
+        >
+          {winnerUser.username}
+        </h4>
+      </div>
+      <div className={MatchCardStyle["match-score-container"]}>
+        <h4 className={MatchCardStyle["match-winorlose"]}>{winOrLose}</h4>
+        <h3 className={MatchCardStyle["match-score"]}>
           {props.match.score1} - {props.match.score2}
         </h3>
-        <div className={MatchCardStyle["foe-id"]}>
-          <img src={loserUser.picture} alt={loserUser.username} />
-          <h4>{loserUser.username}</h4>
-        </div>
+      </div>
+      <div className={MatchCardStyle["loser-container"]}>
+        <img
+          className={`${MatchCardStyle["loser-picture"]} ${MatchCardStyle["match-picture"]}`}
+          src={loserUser.picture}
+          alt={loserUser.username}
+        />
+        <h4
+          className={`${MatchCardStyle["loser-name"]} ${MatchCardStyle["match-name"]}`}
+        >
+          {loserUser.username}
+        </h4>
       </div>
     </div>
   );
