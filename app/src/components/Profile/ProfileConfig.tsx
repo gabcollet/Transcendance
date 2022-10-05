8c9e743aad47c4e49cfb9bc50625fa088d3c7a32
LPAube <lpaube@hotmail.com>
2022-10-05 10:28:04 -0400

    Added twofa toggle in profile config

--- app/src/components/Profile/ProfileConfig.tsx
+++ app/src/components/Profile/ProfileConfig.tsx
@@ -1,13 +1,22 @@
 import Cookies from "js-cookie";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProfileContext } from "../../App";
 import styles from "./ProfileConfig.module.css";
 import axios from "axios";
+import { fetchObject } from "./FetchValue";
 
 export const ProfileConfig = (props: any) => {
   const profileName = useContext(ProfileContext);
   const [newDisplayName, setNewDisplayName] = useState("");
   const [newProfilePicture, setNewProfilePicture] = useState(Object);
+  const [newTwoFA, setNewTwoFA] = useState(Object);
+  const [profileUser, setProfileUser] = useState(Object);
+  const [twoFAToggle, setTwoFAToggle] = useState(false);
+
+  useEffect(() => {
+    fetchObject("users/" + profileName, setProfileUser);
+    console.log("useEffect called");
+  }, [twoFAToggle, profileName]);
 
   const handleDisplayNameChange = (event: any) => {
     setNewDisplayName(event.target.value);
@@ -66,33 +75,61 @@ export const ProfileConfig = (props: any) => {
     );
   };
 
+  const handleTwoFAClick = async (event: any) => {
+    await fetchObject("auth/TwoFA/toggle", setNewTwoFA);
+    twoFAToggle === true ? setTwoFAToggle(false) : setTwoFAToggle(true);
+  };
+
   return (
     <section className={styles["config-container"]}>
       <h2 className={styles["config-header"]}>User Configuration</h2>
-      <div className={styles["config-displayname"]}>
+      <div className={styles["config-displayname-container"]}>
         <h3
           className={`${styles["config-displayname-header"]} ${styles["config-subheader"]}`}
         >
           Change Display Name
         </h3>
-        <button onClick={handleNameButtonClick}>Change Display Name</button>
-        <input type="text" onChange={handleDisplayNameChange} />
+        <div className={styles["config-name-button-container"]}>
+          <input type="text" onChange={handleDisplayNameChange} />
+          <button
+            className={styles["config-button"]}
+            onClick={handleNameButtonClick}
+          >
+            Change Display Name
+          </button>
+        </div>
       </div>
-      <div className={styles["config-picture"]}>
+      <div className={styles["config-picture-container"]}>
         <h3
           className={`${styles["config-picture-header"]} ${styles["config-subheader"]}`}
         >
           Change Profile Picture
         </h3>
-        <input type="file" onChange={handleFileSelected} />
-        <button onClick={handleFileUpload}>Upload File</button>
+        <div className={styles["config-image-button-container"]}>
+          <input type="file" onChange={handleFileSelected} />
+          <button
+            className={styles["config-button"]}
+            onClick={handleFileUpload}
+          >
+            Change Profile Picture
+          </button>
+        </div>
       </div>
-      <div className={styles["config-twofa"]}>
+      <div className={styles["config-twofa-container"]}>
         <h3
           className={`${styles["config-twofa-header"]} ${styles["config-subheader"]}`}
         >
           Two Factor Authentication
         </h3>
+        <div className={styles["config-twofa-button-container"]}>
+          <button
+            className={styles["config-button"]}
+            onClick={handleTwoFAClick}
+          >
+            {profileUser.twoFAEnabled && "Deactivate Two Factor Authentication"}
+            {!profileUser.twoFAEnabled && "Activate Two Factor Authentication"}
+          </button>
+        </div>
       </div>
     </section>
   );
